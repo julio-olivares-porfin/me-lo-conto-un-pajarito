309941b16a51ad4caf37b9af51703e7daa3cec19
julio-olivares-porfin <julio@por-fin.cl>
2024-06-16 22:33:26 -0400

    correciones visuales

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="full-container">
-        <div className="header">
-          <h1> Me lo contó un pajarito</h1>
-          <p>Es una forma de decir que uno se enteró de algo, generalmente un rumor o un dato confidencial, sin revelar la fuente exacta de esa información. <span className="fw-bold">Ahora cuando te pidan la fuente, puedes citar un ave chilena</span></p>
-          <p>Por ejemplo: <span className="ejemplo-italic">Chincol, Gaviota, Pequén, etc</span></p>
-        </div>
-        <div className="mi-api">
-          <MiApi />
-        </div>
-        <Footer />
+     <div className="header">
+       <h1> Me lo contó un pajarito</h1>
+       <p>Es una forma de decir que uno se enteró de algo, generalmente un rumor o un dato confidencial, sin revelar la fuente exacta de esa información. <span className="fw-bold">Ahora cuando te pidan la fuente, puedes citar un ave chilena</span></p>
+       <p>Por ejemplo: <span className="ejemplo-italic">Chincol, Gaviota, Pequén, etc</span></p>
+     </div>
+     <div className="mi-api">
+       <MiApi />
+     </div>
+     <Footer />
     </div>
   );
 }
